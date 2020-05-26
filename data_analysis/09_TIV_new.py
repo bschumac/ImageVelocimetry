1ea3884cd0ba5b2d07f268a8a3887b5597ae71c4
bschumac <benjamin-schumacher@gmx.de>
2020-05-27 11:43:44 +1200

    added assimilation to TIV and parallel processing

--- data_analysis/09_TIV_new.py
+++ data_analysis/09_TIV_new.py
@@ -30,7 +30,7 @@ import skimage
 
 ### USER INPUT ###
 
-experiment = "T2"
+experiment = "T1"
 
 
 
@@ -44,6 +44,7 @@ if experiment == "T0":
     file = h5py.File(datapath+'Tb_stab_rect_pertub_py.nc','r')
 elif experiment == "T1":
     datapath = "/home/benjamin/Met_ParametersTST/T1/Tier02/12012019/Optris_data/"
+    outpath= "/mnt/Seagate_Drive1/T1/"
     file = h5py.File(datapath+'Tb_stab_cut_red_20Hz.nc','r')
     mean_time = 0
     subsample = 10 
@@ -87,13 +88,12 @@ if hard_subsample:
     tb = create_tst_subsample(tb, subsample)
 elif subsample != 0:
     tb = create_tst_subsample_mean(tb, subsample)
-    
-
-pertub = create_tst_pertubations_mm(tb)
-
-
+ 
 if mean_time != 0:
     tb = create_tst_mean(tb,mean_time) 
+
+   
+
 
 
 
@@ -117,64 +117,94 @@ ret_lst = randomize_find_interval(data = tb,rec_freq = 2)
 #writeNetCDF(datapath, "Tb_stab_cut_red_3Hz_120s_meantime_0_hardsubsample_"+str(hard_subsample)+"_pertub.nc", "Tb_pertub", pertub)
 
 
+def runATIV (tb, time_interval, mean_time, outpath, ws, ol, sa, olsa, subsample, hard_subsample, pertubtime, set_len = None, method= "greyscale", my_dpi=300):
+    
+    pertub = create_tst_pertubations_mm(tb,pertubtime)
+    
+    outpath = (outpath+"tiv/experiment_"+experiment+"_meantime_"+str(mean_time)+"_interval_"+str(time_interval)+"_method_"+method+"_WS_"+
+    str(ws)+"_OL_"+str(ol)+"_SA_"+str(sa)+"_SAOL_"+str(olsa)+"_subsample_"+str(subsample)+"_hard_subsample_"+str(hard_subsample)+"_pertubtime"+str(pertubtime)+"/")
+    
+    if not os.path.exists(outpath):
+        os.makedirs(outpath)
+            
+    print(outpath)
+
+
+
+
+    len_pertub = len(pertub)
+    if set_len is not None:
+        len_pertub = set_len
+        
+    
+    for i in range(0, len_pertub-time_interval):
+        
+        print(i)
+        
+        
+        u, v= window_correlation_tiv(frame_a=pertub[i], frame_b=pertub[i+time_interval], window_size_x=ws, overlap_window=ol, overlap_search_area=olsa, 
+                                     search_area_size_x=sa, corr_method=method, mean_analysis = False, std_analysis = False, std_threshold = 10)
+        x, y = get_coordinates( image_size=pertub[i].shape, window_size=sa, overlap=olsa )      
+        
+        u = remove_outliers(u,filter_size=9, sigma=1)
+        v = remove_outliers(v, filter_size=9, sigma=1)
+        
+        
+        plt.imshow(pertub[i], vmin = -1, vmax=1)
+        plt.colorbar()
+        plt.quiver(x,y,np.flipud(np.round(u,2)),np.flipud(np.round(v,2)))
+        plt.savefig(outpath+'{:04d}'.format(i)+".png",dpi=my_dpi,bbox_inches='tight',pad_inches = 0,transparent=False)
+        plt.close()
+        
+        
+        #plt.imshow(u)
+        
+        v= np.reshape(v,((1,v.shape[0],v.shape[1])))
+        u= np.reshape(u,((1,u.shape[0],u.shape[1])))
+    
+        if i == 0:
+            uas =  copy.copy(u)
+            vas =  copy.copy(v)
+    
+        else:
+            uas = np.append(uas,u,0)
+            vas = np.append(vas,v,0)
+    writeNetCDF(outpath, 'UAS.netcdf', 'u', uas)
+    writeNetCDF(outpath, 'VAS.netcdf', 'v', vas)
+    return()
+    
 
 
 
 
 
-method = "greyscale"
-my_dpi = 300
+
+
 time_interval = int(ret_lst[0])
-
-
 ws=16
 ol = 15
-sa = 48
-olsa = 40
+sa = 32
+olsa = 28
+
+time_lst = [80, 60, 40, 20, 10, 5]
 
 
-outpath = (outpath+"tiv/experiment_"+experiment+"_meantime_"+str(mean_time)+"_interval_"+str(time_interval)+"_method_"+method+"_WS_"+
-str(ws)+"_OL_"+str(ol)+"_SA_"+str(sa)+"_SAOL_"+str(olsa)+"_subsample_"+str(subsample)+"_hard_subsample_"+str(hard_subsample)+"/")
+from joblib import Parallel, delayed
+Parallel(n_jobs=6)(delayed(runATIV)(tb=tb, time_interval=time_interval, mean_time=mean_time, outpath=outpath, ws = 16, ol = 15, sa = 32, olsa = 28, 
+            subsample = subsample , hard_subsample = hard_subsample, pertubtime = time, set_len = None)for time in time_lst)
 
-if not os.path.exists(outpath):
-    os.makedirs(outpath)
-        
-print(outpath)
 
-#pertub = tb
+Parallel(n_jobs=num_cores)(delayed(processInput)(n, pertub, iterx = max_displacement+inter_window_size -1,itery = max_displacement+inter_window_size -1 , maxval = 198, ttest= True) for n in range(begin_value_images-begin_value_images,end_value_images-begin_value_images-1))
 
-for i in range(0, len(pertub)-time_interval):
-    
-    print(i)
-    
-    
-    u, v= window_correlation_tiv(frame_a=pertub[i], frame_b=pertub[i+time_interval], window_size_x=ws, overlap_window=ol, overlap_search_area=olsa, 
-                                 search_area_size_x=sa, corr_method=method, mean_analysis = False, std_analysis = False, std_threshold = 10)
-    x, y = get_coordinates( image_size=pertub[i].shape, window_size=sa, overlap=olsa )      
-    
-    u = remove_outliers(u,filter_size=9, sigma=1)
-    v = remove_outliers(v, filter_size=9, sigma=1)
-    
-    
-    plt.imshow(pertub[i], vmin = -1, vmax=1, cmap = "gist_rainbow_r")
-    plt.colorbar()
-    plt.quiver(x,y,np.flipud(np.round(u,2)),np.flipud(np.round(v,2)))
-    plt.savefig(outpath+'{:04d}'.format(i)+".png",dpi=my_dpi,bbox_inches='tight',pad_inches = 0,transparent=False)
-    plt.close()
-    
-    
-    #plt.imshow(u)
-    
-    v= np.reshape(v,((1,v.shape[0],v.shape[1])))
-    u= np.reshape(u,((1,u.shape[0],u.shape[1])))
 
-    if i == 0:
-        uas =  copy.copy(u)
-        vas =  copy.copy(v)
 
-    else:
-        uas = np.append(uas,u,0)
-        vas = np.append(vas,v,0)
+
+
+
+
+
+
+
 
 
 
@@ -200,11 +230,6 @@ for i in range(0, len(pertub)-time_interval):
         vas = np.append(vas,v,0)
     
 
-
-
-writeNetCDF(outpath, 'UAS.netcdf', 'u', uas)
-
-writeNetCDF(outpath, 'VAS.netcdf', 'v', vas)
 
 writeNetCDF(outpath, 'WS.netcdf', 'ws', calcwindspeed(uas,vas)*0.2)
 
